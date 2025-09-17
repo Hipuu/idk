122e4f9d4f1e5552ccb0d0f652a70a592052aa0d
Hipuu <thaivuhong09@gmail.com>
2025-09-18 01:55:17 +0700

    Update Spam.php

--- Spam.php
+++ Spam.php
@@ -1,6 +1,6 @@
 <?php
 header('content-type: application/json');
-$sdt = $_GET["phone"];
+$sdt = $_GET["phone"] ?? ($argv[1] ?? null);
 if(strlen($sdt) < 10){
 	echo ECHOJSON(array("status" => "error","msg" => "Vui Lòng Nhập Đúng Số Phone")); exit;
 } else if(!$sdt){
